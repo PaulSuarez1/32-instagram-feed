7ce47e21fd48b68a8b9e435bc6a763514171054a
Paul Suarez <paulsblogging.com@gmail.com>
2018-08-22 19:32:55 -0700

    got my constructor and data finished working on main activity next

--- app/src/main/java/com/example/paulsuarez/downloadedimagelist/InstaAdapter.java
+++ app/src/main/java/com/example/paulsuarez/downloadedimagelist/InstaAdapter.java
@@ -1,4 +1,78 @@
 package com.example.paulsuarez.downloadedimagelist;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.koushikdutta.ion.Ion;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InstaAdapter {
+
+    private List<InstaPost> mFeed;
+
+    public InstaAdapter() {
+        mFeed = new ArrayList<>();
+    }
+
+    public InstaAdapter(List<InstaPost> feed) {
+        mFeed = feed;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.insta_feed_post, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(view);
+        return vh;
+    }
+
+    // reference
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        InstaPost post = mFeed.get(position);
+        holder.bind(post);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mFeed.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        View mView;
+
+        public ImageView mImage;
+        public TextView mTitle;
+        public TextView mDescription;
+
+        private InstaPost mPost;
+
+        public MyViewHolder(View view) {
+            super(view);
+            mView = view;
+
+            mImage = view.findViewById(R.id.image);
+            mTitle = view.findViewById(R.id.title);
+            mDescription = view.findViewById(R.id.description);
+        }
+
+        public void bind(InstaPost post) {
+            mPost = post;
+            mTitle.setText(post.title);
+            mDescription.setText(post.description);
+
+            Ion.with(mImage)
+                    .placeholder(R.drawable.loading)
+                    .load(mPost.imageUrl);
+        }
+    }
+
 }
--- app/src/main/java/com/example/paulsuarez/downloadedimagelist/InstaData.java
+++ app/src/main/java/com/example/paulsuarez/downloadedimagelist/InstaData.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 public class InstaData {
 
-    public static final List<InstaPost> slothprovider = new ArrayList<>();
+    public static final List<InstaPost> hearthstoneCards = new ArrayList<>();
     static {
-        slothprovider.add(new InstaPost("name", "title", "img"));
-        slothprovider.add(new InstaPost("name", "title", "img"));
-        slothprovider.add(new InstaPost("name", "title", "img"));
+
+        hearthstoneCards.add(new InstaPost("Golemagg", "50 mana 20 attack 20 health. Costs (1) less for each damage your hero has taken.", ""));
+        hearthstoneCards.add(new InstaPost("C'Thun", "10 mana 6 attack 6 health. Deal damage equal to this minion's Attack randomly split among all enemies.", ""));
+        hearthstoneCards.add(new InstaPost("Deathwing, Dragonlord", "10 mana 12 attack 12 health. Put all Dragons from your hand into the battlefield.", ""));
+
     }
 }
